10fe7e02201c529d9e4676cec0114c4716c9ecd1
MultiMouse <olmos.luisd@outlook.com>
2023-09-29 20:03:51 -0500

    added functionality to button, started on 5day forecast, adjusted index and styles

--- assets/script.js
+++ assets/script.js
@@ -10,13 +10,99 @@
 // THEN I am again presented with current and future conditions for that city
 
 var currentDay = dayjs().format("(MM/DD/YYYY)");
+var tomorrowDay = dayjs().add(1, "day").format("MM/DD/YYYY");
 
 var cityInput = $("#city-input");
 
 var searchBtn = $("#search-button");
+
+var laterForecastEl = $("#5-day-heading");
 
 var searchHistory = $("#search-history");
 var displayResult = $("#display-result");
 
 
 var savedCities = []; //for local storage eventually
+
+searchBtn.click (function() {
+    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.val() + "&appid=282d710887f9d5d5a0f6834957e54e94";
+    console.log(requestURL);
+
+    fetch(requestURL).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+
+        var cityHeadingEl = $("#city-name");
+        var cityTempEl = $("#temp");
+        var cityWindEl = $("#wind");
+        var cityHumidityEl = $("#humidity");
+
+        var degree = (data.list[0].main.temp - 273.15) * 1.8 + 32; //kelvin to fahrenheit formula
+        var degreeConvert = (Math.round(degree * 100) / 100).toFixed(2);
+
+        var weather = data.list[0].weather[0].main;
+        var weatherIcon;
+
+        var cityName = data.city.name;
+        var cityWind = data.list[0].wind.speed;
+        var cityHumidity = data.list[0].main.humidity;
+
+        if (weather == "Thunderstorm") {
+            weatherIcon = "🌩";
+        } else if (weather == "Drizzle") {
+            weatherIcon = "🌧"
+        } else if (weather == "Rain") {
+            weatherIcon = "⛆"
+        } else if (weather == "Snow") {
+            weatherIcon = "🌨"
+        } else if (weather == "Clear") {
+            weatherIcon = "🌣"
+        } else if (weather == "Clouds") {
+            weatherIcon = "🌤"
+        };
+
+        cityHeadingEl.text(cityName + " " + currentDay + " " + weatherIcon);
+        cityTempEl.text("Temp: " + degreeConvert + "°F");
+        cityWindEl.text("Wind: " + cityWind + " MPH");
+        cityHumidityEl.text("Humidity: " + cityHumidity + "%");
+        displayResult.attr("class", "border p-3");
+
+        //show 5day forecast
+
+        laterForecastEl.text("5-Day Forecast:");
+
+        $(".card").css("display", "block");
+
+        var laterDateEl = $("#day-1 #later-date");
+        var laterIconEl = $("#day-1 #later-icon");
+        var laterTempEl = $("#day-1 #later-temp");
+        var laterWindEl = $("#day-1 #later-wind");
+        var laterHumidityEl = $("#day-1 #later-humidity");
+
+        var laterWeather = data.list[1].weather[0].main;
+        var laterDegree = (data.list[1].main.temp - 273.15) * 1.8 + 32;
+        var laterDegreeConvert = (Math.round(laterDegree * 100) / 100).toFixed(2);
+        var laterWind = data.list[1].wind.speed;
+        var laterHumidity = data.list[1].main.humidity;
+
+        if (laterWeather == "Thunderstorm") {
+            weatherIcon = "🌩";
+        } else if (laterWeather == "Drizzle") {
+            weatherIcon = "🌧"
+        } else if (laterWeather == "Rain") {
+            weatherIcon = "⛆"
+        } else if (laterWeather == "Snow") {
+            weatherIcon = "🌨"
+        } else if (laterWeather == "Clear") {
+            weatherIcon = "🌣"
+        } else if (laterWeather == "Clouds") {
+            weatherIcon = "🌤"
+        };
+
+        laterDateEl.text(tomorrowDay);
+        laterIconEl.text(weatherIcon);
+        laterTempEl.text("Temp: " + laterDegreeConvert + "°F");
+        laterWindEl.text("Wind: " + laterWind + " MPH");
+        laterHumidityEl.text("Humidity: " + laterHumidity + "%");
+    });
+});
